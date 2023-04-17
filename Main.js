cccd47601a8d5550c2c136a74ca19ea184208bd5
r0uven <roma-wolf2002@mail.ru>
2023-04-17 23:56:32 +0300

    Finished objectification(Готовая объективизация)

--- Main.js
+++ Main.js
@@ -7,15 +7,15 @@ const ArifmeticAlphabet = '*/-+'
 
 
 function LeftOrRight(LeftOperator,RightOperator){
+    let test1 = ExpressionTemp[LeftOperator]=="-"||ExpressionTemp[LeftOperator]=="+" 
+    let test2 = ExpressionTemp[RightOperator]=="/"||ExpressionTemp[RightOperator]=="*"
+    
     if(ExpressionTemp[LeftOperator]=="*"||ExpressionTemp[LeftOperator]=="/")
     {
         return 1;//правый
     }
-    else if(ExpressionTemp[LeftOperator]=="-"||ExpressionTemp[LeftOperator]=="+" && ExpressionTemp[RightOperator]=="/"||ExpressionTemp[RightOperator]=="*")
+    else if(test1 && test2) // Работает только так почему-то
     {
-        let test1 = ExpressionTemp[LeftOperator]=="-"||ExpressionTemp[LeftOperator]=="+" 
-        let test2 = ExpressionTemp[RightOperator]=="/"||ExpressionTemp[RightOperator]=="*"
-        let test3 = test1 && test2
         return 2;//левый
     }
     else if(ExpressionTemp[LeftOperator] == "(")
@@ -51,15 +51,14 @@ function ObjectFormation(){
     let ObjectFormationOut = new ObjectMoi()
     if(FirstBracket != undefined && SecondBracket != undefined)// Если скобки есть
     {
-        for (let i = 0; i < ArifmeticAlphabet.length; i+2) {
+        for (let i = 0; i < ArifmeticAlphabet.length; i=i+2) {
             for (let j = FirstBracket+1; j < SecondBracket; j++) {
                 if(ArifmeticAlphabet[i]==ExpressionTemp[j]||ArifmeticAlphabet[i+1]==ExpressionTemp[j]){
-                    ObjectFormationOut.LeftIdentificator = ExpressionTemp[j-1];//Удаление операций типа a+b, a*b и т.д. 
-                    ObjectFormationOut.ArifmeticOperation = ExpressionTemp[j];
-                    ObjectFormationOut.RightIdentificator = ExpressionTemp[j+1];
-                    i = 5;
-                    if(SecondBracket-FirstBracket!=3)
+                    if(SecondBracket-FirstBracket-1!=3)
                     {
+                        ObjectFormationOut.LeftIdentificator = ExpressionTemp[j-1];//Удаление операций типа a+b, a*b и т.д. 
+                        ObjectFormationOut.ArifmeticOperation = ExpressionTemp[j];
+                        ObjectFormationOut.RightIdentificator = ExpressionTemp[j+1];
                         IndexOfObject = j-1;
                         ExpressionTemp[j-1] = ObjectFormationOut
                         ExpressionTemp.splice(j, 2)
@@ -87,7 +86,7 @@ function ObjectFormation(){
                         ObjectFormationOut.RightIdentificator = ExpressionTemp[FirstBracket+3];
                         ExpressionTemp[FirstBracket] = ObjectFormationOut
                         ExpressionTemp.splice(FirstBracket+1, 4)//
-                        switch(LeftOrRight(FirstBracket-1,SecondBracket+1))
+                        switch(LeftOrRight(FirstBracket-1,FirstBracket+1))
                         {
                             case 1:
                                 ObjectForTree.RightIdentificator = ObjectFormationOut;
@@ -103,19 +102,19 @@ function ObjectFormation(){
                                 break;
                         }
                     }
+                    return;
                 }
             }
         }
         
     }
     else{   //Если скобки не оказалось
-        for (let i = 0; i < ArifmeticAlphabet.length; i+2) {
+        for (let i = 0; i < ArifmeticAlphabet.length; i=i+2) {
             for (let j = 0; j < ExpressionTemp.length; j++) {
                 if(ArifmeticAlphabet[i]==ExpressionTemp[j]||ArifmeticAlphabet[i+1]==ExpressionTemp[j]){
                     ObjectFormationOut.LeftIdentificator = ExpressionTemp[j-1];
                     ObjectFormationOut.ArifmeticOperation = ExpressionTemp[j];
                     ObjectFormationOut.RightIdentificator = ExpressionTemp[j+1];
-                    i = 5;
                     IndexOfObject = j-1;
                     ExpressionTemp[j-1] = ObjectFormationOut
                     ExpressionTemp.splice(j, 2)
@@ -134,6 +133,7 @@ function ObjectFormation(){
                             ObjectForTree.RightIdentificator = ObjectFormationOut;
                             break;
                     }
+                    return;
                 }
             }
         }
@@ -158,8 +158,17 @@ document.querySelector("#Process").onclick = function(){
         ExpressionTemp = Array.from(ExpressionTemp);
     }
     while (ExpressionTemp.length !=1) {
-        ObjectFormation()
+        if(ExpressionTemp.length ==3)
+        {
+            ObjectForTree.RightIdentificator = ExpressionTemp[0]
+            ObjectForTree.ArifmeticOperation = ExpressionTemp[1]
+            ObjectForTree.LeftIdentificator = ExpressionTemp[2]
+            ExpressionTemp.length = 1;
+        }
+        else
+        {
+            ObjectFormation()
+        }
     }
-    
     //console.log(kas.Str)
 }
